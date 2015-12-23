0df542a25590af7474ab629d941ac96262bd4f40
ragg <Ragg-@users.noreply.github.com>
2015-12-23 20:33:56 +0900

    `maya` command now exit if execute on non maya.js project.
    For safety against unintended file rewrite.
    
    Below commands required maya.js project directory structure.
    - cqc
    - export
    - generate (g)
    
    if this commands execute on non maya.js project structure.
    There is a possibility to perform a user of unintended file rewrite / create.

--- src-server/cli/entry.js
+++ src-server/cli/entry.js
@@ -1,4 +1,34 @@
-var [, , command, ...args] = process.argv;
+import fs from "fs";
+import path from "path";
+
+// Check valid maya.js project
+function exitIfInvalidMayaProject() {
+    const appRoot = process.cwd();
+
+    try {
+        const packageJson = require(path.join(appRoot, "package.json"));
+        const deps = packageJson.dependencies;
+        const devDeps = packageJson.devDependencies;
+
+        if (deps && Object.keys(deps).indexOf("maya") !== -1) {
+            return;
+        }
+
+        if (devDeps && Object.keys(devDeps).indexOf("maya") !== -1) {
+            return;
+        }
+
+        console.error("\u001b[31mHere is not maya.js project. \n(reliance on maya.js can not be found in `dependencies` or `devDependencies`)\u001b[m");
+    }
+    catch (e) {
+        console.error("\u001b[31mHere is not maya.js project. \n(package.json not found.)\u001b[m");
+    }
+
+    process.exit(-1);
+}
+
+
+const [, , command, ...args] = process.argv;
 
 switch (command) {
     case "init":
@@ -6,15 +36,18 @@ switch (command) {
         break;
 
     case "cqc":
+        exitIfInvalidMayaProject();
         require("./cqc")(args);
         break;
 
     case "export":
+        exitIfInvalidMayaProject();
         require("./export")(args);
         break;
 
     case "g":
     case "generate":
+        exitIfInvalidMayaProject();
         require("./generate")(args);
         break;
 
