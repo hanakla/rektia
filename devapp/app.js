892cb8d9640554a2c00a2fb1eb8364b8ef8d01cd
ragg <Ragg-@users.noreply.github.com>
2016-01-04 06:43:32 +0900

    Split external middleware into app/app.js

--- devapp/app.js
+++ devapp/app.js
@@ -7,5 +7,13 @@ var app = new Maya(Object.assign(options, {
 
 // If you want to use middleware.
 // Write `app.use(middleware)` here.
+app.use(require("body-parser"));
+app.use(require("cookie-parser"));
+app.use(require("express-session")({
+    resave : false,
+    saveUninitialized : false,
+    secret : app.config.get("session.secret"),
+}));
+app.use(require("method-override"));
 
 app.start();
