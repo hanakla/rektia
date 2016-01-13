15d7d8e4085f89c966a3592e59cf4574b534f696
ragg <Ragg-@users.noreply.github.com>
2016-01-13 14:39:39 +0900

    Remove unused middlewares

--- src-server/middleware/io-watch-assets.js
+++
@@ -1,7 +0,0 @@
-export default function ioWatchAssets() {
-    return (socket, next) => {
-        // join socket into maya.js messaging room.
-        socket.join("__maya__");
-        next();
-    };
-}
