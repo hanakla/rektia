c17a9211de853bc2dfccaf3a48bf8bcee8d53d97
ragg <Ragg-@users.noreply.github.com>
2016-01-13 17:13:40 +0900

    Implemet socket example

--- devapp/static/scripts/socket.js
+++ devapp/static/scripts/socket.js
@@ -1,10 +1,36 @@
-const io = require("socket.io");
-const socket = io.connect();
+const _ = require("lodash");
 
-// export to global
-window.io = io;
-window.socket = socket;
+const messageTemplate = _.template(`
+    <small class="message-says"><%= message.user %></small>
+    <div class="message-body"><%= message.body %></small>
+`);
 
-socket.on("connect", () => {
-    console.log("socket connected!", socket);
+window.addEventListener("DOMContentLoaded", function () {
+    const io = require("socket.io");
+    const socket = io.connect();
+
+    const messageList = document.getElementById("messageList");
+    const messageInput = document.getElementById("messageInput");
+
+    // export to global
+    window.io = io;
+    window.socket = socket;
+
+    socket.on("connect", () => {
+        console.log("socket connected!", socket);
+
+        messageInput.addEventListener("keydown", e => {
+            if (e.keyCode === 13 /* Enter */)  {
+                socket.emit("message", {message: messageInput.value});
+                messageInput.value = "";
+            }
+        });
+
+        socket.on("receive-message", message => {
+            const messageEl = document.createElement("li");
+            messageEl.className = "message";
+            messageEl.innerHTML = messageTemplate({message});
+            messageList.appendChild(messageEl);
+        });
+    });
 });
