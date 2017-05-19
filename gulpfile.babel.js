a75900d9de4b839276500665331a8c7c282aec8b
Ragg <Ragg-@users.noreply.github.com>
2017-05-19 21:20:26 +0900

    Move fly.js to gulp4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,10 @@
-const fs = require("fs");
+import g from 'gulp'
+import rmfr from 'rmfr'
+import fs from 'fs'
+import webpack from 'webpack'
+import pug from 'gulp-pug'
+import {spawn} from 'child_process'
+
 const paths = {
     serverDist : "./lib/",
     binDist : "./lib/bin/",
@@ -6,133 +12,27 @@ const paths = {
     browserDist : "./",
 }
 
-export default function* () {
-    yield this.watch("src-server/**/*", ["buildServer"]);
-    yield this.watch("src-browser/**/*", ["buildBrowser"]);
-}
-
-export function* prePubulish() {
-    yield this.start(["buildServer"]);
-}
-
-export function* buildServer() {
-    yield this.start(["cleanServer", "babelServer", "jadeServer", "copyBin"]);
-}
-
-export function* buildBrowser() {
-    yield this.start(["cleanBrowser", "babelBrowser"]);
-}
-
-
-// Browser side script tasks
-
-export function* cleanBrowser() {
-    yield this.clear(paths.browserDistTmp);
-    yield this.clear(`${paths.browserDist}/maya.js`);
-}
-
-// export function* jadeServer() {
-//     yield this
-//         .source("src/**/*.jade")
-//         // .jade({base: "src/views"})
-//         .target(paths.serverDist);
-// }
-
-export function* babelBrowser() {
-    yield this
-        .source("src-browser/**/*.js")
-        .babel({
-            presets : [
-                "stage-3"
-            ],
-            plugins : [
-                "transform-es2015-arrow-functions",
-                "transform-es2015-block-scoped-functions",
-                "transform-es2015-block-scoping",
-                "transform-es2015-classes",
-                "transform-es2015-computed-properties",
-                "transform-es2015-destructuring",
-                "transform-es2015-for-of",
-                "transform-es2015-function-name",
-                "transform-es2015-literals",
-                "transform-es2015-modules-commonjs",
-                "transform-es2015-object-super",
-                "transform-es2015-parameters",
-                "transform-es2015-shorthand-properties",
-                "transform-es2015-spread",
-                "transform-es2015-sticky-regex",
-                "transform-es2015-template-literals",
-                "transform-es2015-typeof-symbol",
-                "transform-es2015-unicode-regex",
-                "add-module-exports"
-            ]
-        })
-        .target(paths.browserDistTmp);
-
-    yield this.source(`${paths.browserDistTmp}maya.js`)
-        .browse()
-        .uglify()
-        // .notify({
-        //     title : "Fly",
-        //     message : "Compile successful."
-        // })
-        .target(paths.browserDist);
-}
-
 // Server side script tasks
+const compileJade = () =>
+    g.src("src-server/**/*.jade")
+        .pipe(pug())
+        .dest(paths.serverDist)
 
-export function* cleanServer() {
-    yield this.clear(`${paths.serverDist}/**/*`);
+export function compileServerCode() {
+    const proc = spawn(`${__dirname}/node_modules/.bin/tsc`, ['--watch', 'src-server/index.ts'], {stdio: 'inherit'})
 }
 
-export function* jadeServer() {
-    yield this
-        .source("src-server/**/*.jade")
-        // .jade({base: "src/views"})
-        .target(paths.serverDist);
-}
+export async function copyBin() {
+    await g.src("src-server/bin/*").target(paths.binDist)
 
-export function* babelServer() {
-    yield this
-        .source("src-server/**/*.js")
-        .babel({
-            presets : [
-                "stage-3",
-            ],
-            plugins : [
-                "transform-es2015-arrow-functions",
-                "transform-es2015-block-scoped-functions",
-                "transform-es2015-block-scoping",
-                "transform-es2015-classes",
-                "transform-es2015-computed-properties",
-                "transform-es2015-destructuring",
-                "transform-es2015-for-of",
-                "transform-es2015-function-name",
-                "transform-es2015-literals",
-                "transform-es2015-modules-commonjs",
-                "transform-es2015-object-super",
-                "transform-es2015-parameters",
-                "transform-es2015-shorthand-properties",
-                "transform-es2015-spread",
-                "transform-es2015-sticky-regex",
-                "transform-es2015-template-literals",
-                "transform-es2015-typeof-symbol",
-                "transform-es2015-unicode-regex",
-                "add-module-exports"
-            ]
-        })
-        // .notify({
-        //     title : "Fly",
-        //     message : "Compile successful."
-        // })
-        .target(paths.serverDist);
-}
-
-export function* copyBin() {
-    yield this.source("src-server/bin/*")
-        .target(paths.binDist);
-
-    yield new Promise((resolve, reject) => {
+    await new Promise((resolve, reject) => {
         fs.chmod(paths.binDist + "maya", "0744", (err) => err ? reject(err) : resolve());
     });
 }
+
+const cleanServer = () => rmfr(`${paths.serverDist}/**/*`)
+const buildServer = g.series(cleanServer, compileServerCode, copyBin)
+const prePubulish = g.series(buildServer)
+
+export {buildServer, prePubulish, cleanServer, compileJade}
+export default g.series(buildServer)
