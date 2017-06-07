e693fe50a990baf869aa3ec50f8c217bc8d229f5
Ragg <Ragg-@users.noreply.github.com>
2017-06-07 19:19:14 +0900

    Move Controller

--- src-server/index.ts
+++ src-server/index.ts
@@ -2,5 +2,5 @@ import Rektia from './Rektia'
 
 export default Rektia
 export {default as Context} from './Context'
-export {default as Controller} from './Controller'
+export {default as Controller} from './Controller/Controller'
 export {default as Model} from './Model/Model'
