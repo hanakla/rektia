018ea8d8956508e26ae5c16ecf2c1a25c07e25aa
Ragg <Ragg-@users.noreply.github.com>
2017-06-10 02:18:52 +0900

    Ignoring

--- dist/Model/ModelUtil.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _ = require("lodash");
-const pluralize = require("pluralize");
-exports.tableNameFromModel = (model) => {
-    return model.tableName || _.snakeCase(pluralize.plural(model.name));
-};
