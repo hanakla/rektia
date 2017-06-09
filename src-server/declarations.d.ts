212f518e2264e40b803296f1f23fa8b2c2d6047e
Ragg <Ragg-@users.noreply.github.com>
2017-06-10 02:17:47 +0900

    Remove unnecessory packages

--- src-server/declarations.d.ts
+++ src-server/declarations.d.ts
@@ -1,6 +1,1 @@
-declare module 'require-without-cache' {
-    const _: (module: string, require: NodeRequire) => any;
-    export = _
-}
-
 declare module 'module-alias';
--- test.ts
+++
@@ -1,5 +0,0 @@
-import {Controller, default as Rektia} from './src-server/index'
-import koa from 'koa'
-
-const app = new koa()
-app.listen
